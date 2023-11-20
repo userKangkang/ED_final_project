5eb6ec1d59480d6354d4a7d5cded5c374c40d4e9
JingfengS <jingfeng050106@gmail.com>
2023-11-21 00:55:50 +0800

    Finish the searchBox

--- ed_discussion/src/app/homeComponents/searchBox.js
+++ ed_discussion/src/app/homeComponents/searchBox.js
@@ -1,13 +1,40 @@
-import Icons from './icons.module.css';
-import Search from './NextuiIcons/search';
+import React from "react";
+import {Input} from "@nextui-org/react";
+import Search from "@/app/homeComponents/NextuiIcons/search";
 
 export default function SearchBox() {
     return (
-        <>
-        <div className=" relative inset-y-0 left-0 flex items-center px-2">
-        <Search/>
-        <input type="text" className={"ml-2 search w-full h-12 bg-black"} placeholder="Search"/>
+        <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+            <Input
+                radius={"sm"}
+                classNames={{
+                    label: "text-black/50 dark:text-white/90",
+                    input: [
+                        "bg-transparent",
+                        "text-black/90 dark:text-white/90",
+                        "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+                    ],
+                    innerWrapper: "bg-transparent",
+                    inputWrapper: [
+                        "shadow-xl",
+                        "bg-[#180828]/70",
+                        "dark:bg-default/60",
+                        "backdrop-blur-xl",
+                        "backdrop-saturate-200",
+                        "hover:bg-default/70",
+                        "dark:hover:bg-default/70",
+                        "group-data-[focused=true]:bg-default-200/50",
+                        "dark:group-data-[focused=true]:bg-default/60",
+                        "!cursor-text",
+                    ],
+                }}
+                type="search"
+                placeholder="Search"
+                labelPlacement="outside"
+                startContent={
+                <Search />
+                }
+            />
         </div>
-        </>
     );
 }
