e7d1b1c9b7c3f0ad762e5d7fcfdbb8f6d3c0915c
JingfengS <jingfeng050106@gmail.com>
2023-11-20 19:22:33 +0800

    change the size of the color block

--- ed_discussion/src/app/homeComponents/TypeTitle.js
+++ ed_discussion/src/app/homeComponents/TypeTitle.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function TypeTitle({color, name}) {
     return (
         <div className={"flex flex-row align-middle"}>
-            <div className={`w-2 h-2 ${color} mr-2`}></div>
+            <div className={`w-3 h-3 ${color} mr-2`}></div>
             <p>{name}</p>
         </div>
     );
