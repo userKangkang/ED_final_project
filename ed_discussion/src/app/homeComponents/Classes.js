01ff0aa981e21289ee9085d558305badbab661aa
JingfengS <jingfeng050106@gmail.com>
2023-11-21 11:14:41 +0800

    Change the Classes Chip to make it look better

--- ed_discussion/src/app/homeComponents/Classes.js
+++ ed_discussion/src/app/homeComponents/Classes.js
@@ -3,6 +3,6 @@ import {Chip} from "@nextui-org/react";
 
 export default function Classes(){
     return (
-        <Chip className={"mt-3 mx-2"} isDisabled color="default" variant="shadow">类别</Chip>
+        <Chip className={"mt-3 mx-2"} color="secondary" variant="shadow">类别</Chip>
     );
 }
