f7faab620aa9285ec9547dd9acb5f2da05ac58a3
JingfengS <jingfeng050106@gmail.com>
2023-11-23 00:57:37 +0800

    Make the input box able to accept images

--- ed_discussion/src/Components/newPage/textBox.js
+++ ed_discussion/src/Components/newPage/textBox.js
@@ -1,25 +1,48 @@
 "use client"
-import React from "react";
-import {Card, CardHeader, CardBody, Image, Button, Chip, Textarea} from "@nextui-org/react";
+import './styles.scss'
+
+import Document from '@tiptap/extension-document'
+import Dropcursor from '@tiptap/extension-dropcursor'
+import Paragraph from '@tiptap/extension-paragraph'
+import Image from '@tiptap/extension-image'
+import Text from '@tiptap/extension-text'
+import { EditorContent, useEditor } from '@tiptap/react'
+import React, { useCallback } from 'react'
+import {Card, CardHeader, CardBody, Button, Chip, Textarea} from "@nextui-org/react";
 export default function TextBox() {
+    const editor = useEditor({
+        extensions: [Document, Paragraph, Text, Image, Dropcursor],
+        autofocus: true,
+        editable: true,
+        injectCSS: false,
+    });
+
+    const addImage = useCallback(() => {
+        const url = window.prompt('URL')
+
+        if (url) {
+            editor.chain().focus().setImage({src: url}).run()
+        }
+    }, [editor])
+
+    if (!editor) {
+        return null
+    }
+
     return (
         <div className={"flex justify-center mt-5"}>
             <Card className={"py-4 w-full m-3"}>
                 <CardHeader className="pb-0 pt-0 px-4 flex-row justify-between">
                     <Chip color={"default"}>Paragraph</Chip>
-                    <Button isIconOnly={true} size={"sm"} color={"secondary"}>
-                        <Image
+                    <Button isIconOnly={true} size={"sm"} color={"secondary"} onClick={addImage}>
+                        <img
                             src={"/image.svg"}
                             alt={"add image"}
                         />
                     </Button>
                 </CardHeader>
                 <CardBody className={"overflow-visible py-2"}>
-                    <Textarea
-                        variant={"bordered"}
-                        minRows={10}
-                        maxRows={18}
-                        />
+                  <EditorContent editor={editor} />
                 </CardBody>
             </Card>
         </div>
