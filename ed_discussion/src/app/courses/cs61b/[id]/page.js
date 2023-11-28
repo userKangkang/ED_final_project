780a923ed01e017fcfe3db6b4ba40604c54bc3e3
JingfengS <jingfeng050106@gmail.com>
2023-11-28 12:53:04 +0800

    Add more comments

--- ed_discussion/src/app/courses/cs61b/[id]/page.js
+++ ed_discussion/src/app/courses/cs61b/[id]/page.js
@@ -1,6 +1,12 @@
 import {sql} from "@vercel/postgres";
 import getTopDataIds from "@/app/api/routes/topMessage";
 
+/**
+ * You don't need to care about this function
+ * which is only used to generate dynamic routes
+ * If you are interested, you can search generateStaticParams in the nextjs document
+ * @returns {Promise<*>}
+ */
 export async function generateStaticParams() {
     const ids = await getTopDataIds();
     return ids.map((row) => ({
@@ -9,7 +15,14 @@ export async function generateStaticParams() {
     )
 }
 
-export default function Cs61bQuestions({params}) {
+/**
+ * This function will place the content of the middle and right part of the page
+ * The id of the page is provided for you.
+ * @param params
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function Cs61bQuestionsPage({params}) {
     const {id} = params;
     return (
         <div>
