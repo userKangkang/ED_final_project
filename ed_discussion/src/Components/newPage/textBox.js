47538f5d57c0b21de9b357675dfa20af5a5b9475
JingfengS <jingfeng050106@gmail.com>
2023-11-29 17:03:29 +0800

    Enable upload images with a button

--- ed_discussion/src/Components/newPage/textBox.js
+++ ed_discussion/src/Components/newPage/textBox.js
@@ -7,18 +7,19 @@ import Paragraph from '@tiptap/extension-paragraph'
 import Image from '@tiptap/extension-image'
 import Text from '@tiptap/extension-text'
 import { EditorContent, useEditor } from '@tiptap/react'
-import React, { useCallback } from 'react'
+import React, { useCallback, useRef } from 'react'
 import {Card, CardHeader, CardBody, Button, Chip, Textarea} from "@nextui-org/react";
+import {UploadButton} from "@/app/api/uploadthing/uploadbutton"
 export default function TextBox({editor, handleSubmit}) {
-
-
+    const imageUrl = useRef("");
     const addImage = useCallback(() => {
-        const url = window.prompt('URL')
+        console.log(imageUrl.current);
+        const url = imageUrl.current;
 
         if (url) {
             editor.chain().focus().setImage({src: url}).run()
         }
-    }, [editor])
+    }, [editor, imageUrl])
 
     if (!editor) {
         return null
@@ -29,13 +30,20 @@ export default function TextBox({editor, handleSubmit}) {
         <div className={"flex justify-center mt-5"}>
             <Card className={"py-4 w-full m-3"}>
                 <CardHeader className="pb-0 pt-0 px-4 flex-row justify-between">
-                    <Chip color={"default"}>Paragraph</Chip>
-                    <Button isIconOnly={true} size={"sm"} color={"secondary"} onClick={addImage}>
-                        <img
-                            src={"/image.svg"}
-                            alt={"add image"}
-                        />
-                    </Button>
+                    <Chip color={"default"} size={"md"}>Paragraph</Chip>
+                    <UploadButton
+                        endpoint="imageUploader"
+                        onClientUploadComplete={(res) => {
+                            // Do something with the response
+                            imageUrl.current = res[0].url;
+                            console.log(res[0]);
+                            addImage();
+                        }}
+                        onUploadError={(error) => {
+                            // Do something with the error.
+                            alert(`ERROR! ${error.message}`);
+                        }}
+                    />
                 </CardHeader>
                 <CardBody className={"overflow-visible py-2 flex flex-col"}>
                   <EditorContent editor={editor} />
--- ed_discussion/src/app/layout.js
+++ ed_discussion/src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import {Providers} from "@/app/providers";
 import Header from '@/Components/homeComponents/Navbar';
+import "@uploadthing/react/styles.css";
 
 const inter = Inter({ subsets: ['latin'] })
 
