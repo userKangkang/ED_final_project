c721a178b75899f0c2667975dc0f6a91458964e0
JingfengS <jingfeng050106@gmail.com>
2023-12-03 22:13:54 +0800

    Nothing

--- ed_discussion/src/app/profile/page.js
+++ ed_discussion/src/app/profile/page.js
@@ -1,15 +1,13 @@
 import PersonalSet from "@/Components/personalMessage/personalSet";
 import * as React from "react";
-import Header from "@/Components/homeComponents/Navbar";
-import PersonalSideBar from "@/Components/personalMessage/personalSidebar";
 
 
 export default function HomePage() {
   return(
     <>
-        <div className={"w-5/6 border-solid border-l-2 border-gray-400 min-h-screen overflow-y-scroll"}>
+       <div className={"w-5/6 border-solid border-l-2 border-gray-400 min-h-screen overflow-y-scroll"}>
           <PersonalSet/>
-        </div>
+       </div>
     </>
   );
 }
