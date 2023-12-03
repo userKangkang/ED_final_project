248a9c9017b0c7770cb49bc0cf7dece8e32fea99
userKangkang <hwqqz678@sjtu.edu.cn>
2023-12-03 20:57:03 +0800

    Add route of profile and initialize them.

---
+++ ed_discussion/src/app/profile/layout.js
@@ -0,0 +1,21 @@
+import PersonalSideBar from "@/Components/personalMessage/personalSidebar";
+import PersonalSet from "@/Components/personalMessage/personalSet";
+import * as React from "react";
+import Header from "@/Components/homeComponents/Navbar";
+import {Divider} from "@nextui-org/react";
+
+export default function HomePage({children}) {
+  return(
+    <>
+      <Header/>
+      <main className={"flex max-h-screen flex-row items-start p-0 min-w-screen bg-gradient-to-r from-[#0D001A] to-[#180828]"}>
+        <div className={"w-1/6 w-72 border-solid border-r-2 border-gray-400 min-h-screen flex flex-col items-start"}>
+          <PersonalSideBar/>
+        </div>
+        <div className={"w-5/6 border-solid border-l-2 border-gray-400 min-h-screen overflow-y-scroll"}>
+          {children}
+        </div>
+      </main>
+    </>
+  );
+}
