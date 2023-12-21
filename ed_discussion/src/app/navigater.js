fbe424485aa01821ddd8bc3ff2318bf4a2dc431f
JingfengS <jingfeng050106@gmail.com>
2023-12-21 20:34:47 +0800

    Make it possible for page to load css

--- ed_discussion/src/app/navigater.js
+++ ed_discussion/src/app/navigater.js
@@ -2,37 +2,39 @@ import { useState } from "react";
 import {Button} from "@nextui-org/react";
 
 export default async function HomePage() {
-  
+    return (
+        <div className="flex justify-center gap-80 items-center w-full py-8">
+            <Button
+                color="success"
+                onClick={() => {
+                    setSelectedNav("1");
+                    document.getElementById("section1").scrollIntoView({behavior: "smooth"});
+                }}
+                className={selectedNav === "1" ? "text-gray-50 font-bold" : "text-gray-50"}
+            >
+                1
+            </Button>
+            <Button
+                color="primary"
+                onClick={() => {
+                    setSelectedNav("2");
+                    document.getElementById("section2").scrollIntoView({behavior: "smooth"});
+                }}
+                className={selectedNav === "2" ? "text-gray-50 font-bold" : "text-gray-50"}
+            >
+                2
+            </Button>
+            <Button
+                color="warning"
+                onClick={() => {
+                    setSelectedNav("3");
+                    document.getElementById("section3").scrollIntoView({behavior: "smooth"});
+                }}
+                className={selectedNav === "3" ? "text-gray-50 font-bold" : "text-gray-50"}
+            >
+                3
+            </Button>
+        </div>
+
+    )
 }
-<div className="flex justify-center gap-80 items-center w-full py-8">
-  <Button
-    color="success"
-    onClick={() => {
-      setSelectedNav("1");
-      document.getElementById("section1").scrollIntoView({ behavior: "smooth" });
-    }}
-    className={selectedNav === "1" ? "text-gray-50 font-bold" : "text-gray-50"}
-  >
-    1
-  </Button>
-  <Button
-    color="primary"
-    onClick={() => {
-      setSelectedNav("2");
-      document.getElementById("section2").scrollIntoView({ behavior: "smooth" });
-    }}
-    className={selectedNav === "2" ? "text-gray-50 font-bold" : "text-gray-50"}
-  >
-    2
-  </Button>
-  <Button
-    color="warning"
-    onClick={() => {
-      setSelectedNav("3");
-      document.getElementById("section3").scrollIntoView({ behavior: "smooth" });
-    }}
-    className={selectedNav === "3" ? "text-gray-50 font-bold" : "text-gray-50"}
-  >
-    3
-  </Button>
-</div>
--- ed_discussion/tailwind.config.js
+++ ed_discussion/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
       './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
